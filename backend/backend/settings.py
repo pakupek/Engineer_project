498702aabfaa938ff901c10ffae3e5f0e85aa75b
pakupek <adam.akimowicz13@onet.eu>
2025-12-08 18:29:06 +0100

    pobieranie zmiennych celery

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,8 +27,8 @@ MEDIA_ROOT = '/app/backend/media'
 MEDIA_URL = '/media/'
 
 # Celery settings
-CELERY_BROKER_URL = "redis://redis:6379/0"
-CELERY_RESULT_BACKEND = "redis://redis:6379/0"
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
 
 
 
