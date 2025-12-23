98f65aee6340f02c96e7104a252dbf6629876965
pakupek <adam.akimowicz13@onet.eu>
2025-12-23 11:58:52 +0100

    zmiana plików statycznych

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,7 +40,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'car_history','static'),
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
