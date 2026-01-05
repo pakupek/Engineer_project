93f18dfed735e41937b2bfe2ca7a287de0055a7b
pakupek <adam.akimowicz13@onet.eu>
2026-01-05 20:59:37 +0100

    zmiana w celery

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -4,9 +4,6 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')  
 
-app = Celery(
-    'backend',
-    broker=os.environ.get("CELERY_BROKER_URL"),
-    backend=os.environ.get("CELERY_RESULT_BACKEND"),) 
+app = Celery('backend') 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
